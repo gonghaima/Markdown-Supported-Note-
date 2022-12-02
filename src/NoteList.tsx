9f772168b1d807e0af2c800f83a570424e6c1fd3
Steven Gong <gonghaima@hotmail.com>
2022-12-02 22:01:45 +1100

    list available tags

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,14 @@
-import { Button, Row, Col, Stack } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Form, Row, Col, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import ReactSelect from 'react-select';
+import { Tag } from './App';
 
-export default function NoteList() {
+type NoteListProp = {
+    availableTags: Tag[];
+};
+export default function NoteList({ availableTags }: NoteListProp) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   return (
     <>
       <Row>
@@ -17,6 +24,37 @@ export default function NoteList() {
           </Stack>
         </Col>
       </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 }
