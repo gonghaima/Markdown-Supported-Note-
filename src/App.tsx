4d030f33bf79c008640ad0d334535923f2c34dd7
Steven Gong <gonghaima@hotmail.com>
2022-12-02 13:24:16 +1100

    settign up newNote page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import { NewNote } from './NewNote';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
