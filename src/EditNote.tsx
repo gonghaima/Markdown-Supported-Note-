b8c0a2adb26c63e2791325411530303c0f8ec34d
Steven Gong <gonghaima@hotmail.com>
2022-12-04 00:00:45 +1100

    edit has initial value

--- src/EditNote.tsx
+++ src/EditNote.tsx
@@ -3,7 +3,7 @@ import { NoteForm } from './NoteForm';
 import { useNote } from './NoteLayout';
 
 type EditNoteProps = {
-  onSubmit: (id:string, data: NoteData) => void;
+  onSubmit: (id: string, data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
 };
@@ -14,7 +14,10 @@ export function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps) {
     <>
       <h1 className="mb-4">Edit Note</h1>
       <NoteForm
-        onSubmit={data=>onSubmit(note.id, data)}
+        title={note.title}
+        markdown={note.markdown}
+        tags={note.tags}
+        onSubmit={(data) => onSubmit(note.id, data)}
         onAddTag={onAddTag}
         availableTags={availableTags}
       />
