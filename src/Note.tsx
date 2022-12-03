72c4f1d9cfc7d3e77e98b7efaf9b709b65f84670
Steven Gong <gonghaima@hotmail.com>
2022-12-03 15:51:35 +1100

    navigation buttons

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,4 +1,5 @@
-import { Badge, Row, Stack } from 'react-bootstrap';
+import { Badge, Button, Col, Row, Stack } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useNote } from './NoteLayout';
 
 export function Note() {
@@ -6,16 +7,30 @@ export function Note() {
   return (
     <>
       <Row className="mb-4">
-        <h1>{note.title}</h1>
-        {note.tags.length > 0 && (
-          <Stack gap={1} direction="horizontal" className="flex-wrap">
-            {note.tags.map((tag) => (
-              <Badge className="text-truncate" key={tag.id}>
-                {tag.label}
-              </Badge>
-            ))}
+        <Col>
+          <h1>{note.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} direction="horizontal" className="flex-wrap">
+              {note.tags.map((tag) => (
+                <Badge className="text-truncate" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </Link>
+            <Button variant="outline-danger">Delete</Button>
+            <Link to="/">
+              <Button variant="outline-secondary">Back</Button>
+            </Link>
           </Stack>
-        )}
+        </Col>
       </Row>
     </>
   );
